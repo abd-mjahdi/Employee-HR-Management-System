b2e05ad89dfff349f9ecae08a349500c13accfd6
abd-mjahdi <abdelouahabmjahdi@gmail.com>
2026-02-21 23:21:59 +0000

    completed project entity

--- backend/employee-time-tracking/src/main/java/com/example/employeetimetracking/model/entities/Project.java
+++ backend/employee-time-tracking/src/main/java/com/example/employeetimetracking/model/entities/Project.java
@@ -1,4 +1,40 @@
 package com.example.employeetimetracking.model.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="projects")
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="project_name" , length=50 ,nullable = false)
+    private String projectName;
+
+    @Column(name="project_code" , length=50 ,nullable = false)
+    private String projectCode;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="is_active")
+    private Boolean isActive = true;
+
+    @CreationTimestamp
+    @Column(name = "created_at" ,nullable = false ,updatable = false)
+    private LocalDateTime createdAt;
+
 }

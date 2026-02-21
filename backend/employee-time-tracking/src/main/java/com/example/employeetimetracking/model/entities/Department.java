b673d2cdecce043e7e093489e8af4c2051f82b65
abd-mjahdi <abdelouahabmjahdi@gmail.com>
2026-02-21 23:06:27 +0000

    completed department entity

--- backend/employee-time-tracking/src/main/java/com/example/employeetimetracking/model/entities/Department.java
+++ backend/employee-time-tracking/src/main/java/com/example/employeetimetracking/model/entities/Department.java
@@ -1,4 +1,43 @@
 package com.example.employeetimetracking.model.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="departments")
 public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="department_name" , length=50 ,nullable = false)
+    private String departmentName;
+
+    @Column(name="department_code" , length=50 ,nullable = false)
+    private String departmentCode;
+
+    @Column(name="is_active")
+    private Boolean isActive = true;
+
+    @CreationTimestamp
+    @Column(name = "created_at" ,nullable = false ,updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at" ,nullable = false)
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "department")
+    private List<User> users;
 }

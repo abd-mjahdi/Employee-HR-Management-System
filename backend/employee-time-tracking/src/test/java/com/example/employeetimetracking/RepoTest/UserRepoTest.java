577de17e4b08bfbec0be7648fef2d16b860ef4b0
abd-mjahdi <abdelouahabmjahdi@gmail.com>
2026-02-25 21:03:50 +0000

    testing data retrieval from DB

---
+++ backend/employee-time-tracking/src/test/java/com/example/employeetimetracking/RepoTest/UserRepoTest.java
@@ -0,0 +1,44 @@
+package com.example.employeetimetracking.RepoTest;
+
+
+import com.example.employeetimetracking.model.entities.Department;
+import com.example.employeetimetracking.model.entities.User;
+import com.example.employeetimetracking.model.enums.UserRole;
+import com.example.employeetimetracking.repository.DepartmentRepository;
+import com.example.employeetimetracking.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDateTime;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public class UserRepoTest {
+    private final UserRepository userRepository;
+    private final DepartmentRepository departmentRepository;
+
+    @Autowired
+    public UserRepoTest(UserRepository userRepository , DepartmentRepository departmentRepository){
+        this.userRepository =  userRepository;
+        this.departmentRepository = departmentRepository;
+
+    }
+
+
+
+    @Test
+    void testSave(){
+
+        User saved = userRepository.findByUsername("bob.dev");
+
+        System.out.println(saved.getEmail());
+    }
+
+
+
+}

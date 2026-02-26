a42d195f6be74ae3ad177fdcf1d4d7cf390ddeb8
abd-mjahdi <abdelouahabmjahdi@gmail.com>
2026-02-26 22:02:22 +0000

    added new leaveTypeDto and completed LeaveRequestDto

--- backend/employee-time-tracking/src/main/java/com/example/employeetimetracking/dto/response/LeaveRequestDto.java
+++ backend/employee-time-tracking/src/main/java/com/example/employeetimetracking/dto/response/LeaveRequestDto.java
@@ -1,4 +1,26 @@
 package com.example.employeetimetracking.dto.response;
 
+import com.example.employeetimetracking.model.entities.User;
+import com.example.employeetimetracking.model.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LeaveRequestDto {
+    private Long id;
+    private UserDto user;
+    private LeaveTypeDto leaveType;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private BigDecimal totalDays;
+    private String reason;
+    private Status status;
 }

c0630cf09744a5fe69bc3da186ef09b20985c292
abd-mjahdi <abdelouahabmjahdi@gmail.com>
2026-02-26 22:28:45 +0000

    completed leave balance dto

--- backend/employee-time-tracking/src/main/java/com/example/employeetimetracking/dto/request/CreateLeaveRequestDto.java
+++ backend/employee-time-tracking/src/main/java/com/example/employeetimetracking/dto/request/CreateLeaveRequestDto.java
@@ -16,6 +16,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateLeaveRequestDto {
+
     @NotNull
     private Long leaveTypeId;
 
--- backend/employee-time-tracking/src/main/java/com/example/employeetimetracking/dto/response/LeaveBalanceDto.java
+++ backend/employee-time-tracking/src/main/java/com/example/employeetimetracking/dto/response/LeaveBalanceDto.java
@@ -1,4 +1,25 @@
 package com.example.employeetimetracking.dto.response;
 
+import com.example.employeetimetracking.model.entities.LeaveType;
+import com.example.employeetimetracking.model.entities.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LeaveBalanceDto {
+
+    private Long id;
+    private UserDto user;
+    private LeaveTypeDto leaveType;
+    private short year;
+    private BigDecimal currentBalance;
+
+
 }

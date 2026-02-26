c3200bd4ce2d27c3168289a1daa04cc22902bb99
abd-mjahdi <abdelouahabmjahdi@gmail.com>
2026-02-26 22:22:22 +0000

    completed create leave request dto

--- backend/employee-time-tracking/src/main/java/com/example/employeetimetracking/dto/request/CreateLeaveRequestDto.java
+++ backend/employee-time-tracking/src/main/java/com/example/employeetimetracking/dto/request/CreateLeaveRequestDto.java
@@ -1,4 +1,33 @@
 package com.example.employeetimetracking.dto.request;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateLeaveRequestDto {
+    @NotNull
+    private Long leaveTypeId;
+
+    @NotNull
+    @FutureOrPresent
+    private LocalDate startDate;
+
+    @NotNull
+    @Future
+    private LocalDate endDate;
+
+    @NotBlank
+    private String reason;
+
 }

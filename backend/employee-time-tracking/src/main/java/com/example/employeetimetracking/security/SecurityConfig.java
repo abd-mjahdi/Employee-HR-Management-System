3a5818713de90e07065c1b4ebad2db03cf05a631
abd-mjahdi <abdelouahabmjahdi@gmail.com>
2026-03-03 07:03:05 +0000

    security configuration class csrf/cors/filter chain order...

--- backend/employee-time-tracking/src/main/java/com/example/employeetimetracking/security/SecurityConfig.java
+++ backend/employee-time-tracking/src/main/java/com/example/employeetimetracking/security/SecurityConfig.java
@@ -1,21 +1,61 @@
 package com.example.employeetimetracking.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter){
+        this.jwtAuthenticationFilter=jwtAuthenticationFilter;
+    }
+
     @Bean
     public SecurityFilterChain applyConfig(HttpSecurity http) throws Exception{
 
         http.csrf(csrf -> csrf.disable());
+        http.cors(cors->cors.configurationSource(corsConfigurationSource()));
+        http.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.authorizeHttpRequests(auth ->
+                auth
+                        .requestMatchers("/auth/login","/auth/register").permitAll()
+                        .anyRequest().authenticated()
+        );
+        http.addFilterBefore(jwtAuthenticationFilter , UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+
+        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE"));
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedHeaders(List.of("*"));
+
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+
+    }
+
+
 }

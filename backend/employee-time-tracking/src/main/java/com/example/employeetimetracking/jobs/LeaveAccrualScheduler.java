b3875f02dc7df44c2c236ddb0b04f084733019a6
abd-mjahdi <abdelouahabmjahdi@gmail.com>
2026-04-20 21:23:46 +0100

    fix: add missing processing job for annual accrual , and add rollover job

--- backend/employee-time-tracking/src/main/java/com/example/employeetimetracking/jobs/LeaveAccrualScheduler.java
+++ backend/employee-time-tracking/src/main/java/com/example/employeetimetracking/jobs/LeaveAccrualScheduler.java
@@ -4,6 +4,7 @@ import com.example.employeetimetracking.model.entities.LeaveBalance;
 import com.example.employeetimetracking.model.entities.LeavePolicy;
 import com.example.employeetimetracking.model.entities.User;
 import com.example.employeetimetracking.model.enums.AccrualMethod;
+import com.example.employeetimetracking.repository.LeaveBalanceRepository;
 import com.example.employeetimetracking.repository.UserRepository;
 import com.example.employeetimetracking.service.LeaveBalanceService;
 import jakarta.transaction.Transactional;
@@ -15,30 +16,36 @@ import java.util.List;
 
 @Service
 public class LeaveAccrualScheduler {
-    private final UserRepository userRepository;
     private final LeaveBalanceService leaveBalanceService;
+    private final LeaveBalanceRepository leaveBalanceRepository;
     @Autowired
-    public LeaveAccrualScheduler(UserRepository userRepository ,LeaveBalanceService leaveBalanceService){
-        this.userRepository = userRepository;
+    public LeaveAccrualScheduler( LeaveBalanceService leaveBalanceService , LeaveBalanceRepository leaveBalanceRepository){
         this.leaveBalanceService = leaveBalanceService;
+        this.leaveBalanceRepository = leaveBalanceRepository;
     }
     @Scheduled(cron = "0 0 0 1 * ?")
     @Transactional
     public void processMonthlyLeaveAccrual() {
-
-        List<User> users = userRepository.findByIsActive(true);
-
-        for (User user : users) {
-            for (LeaveBalance lb : user.getLeaveBalanceList()) {
-
-                    LeavePolicy policy = lb.getLeaveType().getLeavePolicy();
-
-                    if (policy.getAccrualMethod() == AccrualMethod.ANNUAL) continue;
-
-                    leaveBalanceService.applyMonthlyAccrual(lb);
-                    // log changes in future
-            }
-
-        }
+        List<LeaveBalance> lbs = leaveBalanceRepository.findAllLeaveBalances();
+        lbs.stream()
+                .filter(lb->lb.getLeaveType().getLeavePolicy().getAccrualMethod()==AccrualMethod.MONTHLY)
+                .forEach(leaveBalanceService::applyMonthlyAccrual);
     }
+    @Scheduled(cron = "0 0 0 1 1 ?")
+    @Transactional
+    public void processAnnualLeaveAccrual() {
+        List<LeaveBalance> lbs = leaveBalanceRepository.findAllLeaveBalances();
+        lbs.stream()
+                .filter(lb->lb.getLeaveType().getLeavePolicy().getAccrualMethod()==AccrualMethod.ANNUAL)
+                .forEach(leaveBalanceService::applyAnnualAccrual);
+    }
+
+    @Scheduled(cron = "0 59 23 31 12 *")
+    @Transactional
+    public void yearEndRolloverJob() {
+        List<LeaveBalance> lbs = leaveBalanceRepository.findAllLeaveBalances();
+        lbs.forEach(leaveBalanceService::rolloverBalance);
+
+    }
+
 }
